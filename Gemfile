d36c35b9c393b6e8c4f1af05d91f67733597aed3
Pup Gray <pupgray@outlook.com>
2024-06-11 19:30:21 -0400

    Add minitest-snapshots and update test assertions

--- Gemfile
+++ Gemfile
@@ -12,5 +12,9 @@ gem "sprockets-rails"
 
 gem "nokogiri"
 
+group :test do
+  gem 'minitest-snapshots'
+end
+
 # Start debugger with binding.b [https://github.com/ruby/debug]
 # gem "debug", ">= 1.0.0"
--- test/model_test.rb
+++ test/model_test.rb
@@ -3,96 +3,69 @@
 require_relative "test_helper"
 
 class ModelTest < ActiveSupport::TestCase
+  def setup
+    @view = ActionView::Base.new(ActionView::LookupContext.new([]), {}, nil)
+    @view.extend Fatcow::Helpers
+  end
+
   test 'basic show icon' do
     user = users(:no_status)
     user.save
-    assert_equal user.show_icon.to_s, "<div class=\"fatcow-icon fatcow-icon--user\"><img src=\"/images/fatcow/FatCow_Icons32x32/user.png\"></div>"
+    assert_matches_snapshot @view.fci(user.show_icon).to_s
   end
 
   test 'basic show icon default add state when new record' do
     user = users(:no_status).dup
-    assert_equal user.show_icon.to_s, "<div class=\"fatcow-icon fatcow-icon--user\"><img src=\"/images/fatcow/FatCow_Icons32x32/user_add.png\"></div>"
+    assert_matches_snapshot @view.fci(user.show_icon).to_s
   end
 
   test 'basic show icon default destroyed state when new record' do
     user = users(:no_status)
     user.save
     user.destroy
-    assert_equal user.show_icon.to_s, <<~HTML.chomp
-      <div class="fatcow-icon fatcow-icon--user">
-      <img src="/images/fatcow/FatCow_Icons32x32/user.png"><img class="fatcow-icon__bullet fatcow-icon__bullet--pre-aligned" src="/images/fatcow/FatCow_Icons32x32/bullet_burn.png">
-      </div>
-    HTML
+    assert_matches_snapshot @view.fci(user.show_icon).to_s
   end
 
   test 'basic form icon' do
     user = users(:no_status)
     user.save
-    assert_equal user.form_icon.to_s, "<div class=\"fatcow-icon fatcow-icon--user\"><img src=\"/images/fatcow/FatCow_Icons32x32/user_edit.png\"></div>"
+    assert_matches_snapshot @view.fci(user.form_icon).to_s
   end
 
   test 'custom show icon status' do
     user = users(:name_icon)
     user.save
-    assert_equal user.show_icon.to_s, <<~HTML.chomp
-      <div class=\"fatcow-icon fatcow-icon--user\">
-      <img src=\"/images/fatcow/FatCow_Icons32x32/user.png\"><img class=\"fatcow-icon__bullet\" src=\"/images/fatcow/FatCow_Icons32x32/bullet_star.png\">
-      </div>
-    HTML
+    assert_matches_snapshot @view.fci(user.show_icon).to_s
   end
 
   test 'custom form icon status' do
     user = users(:special_icon)
     user.save
-    assert_equal user.form_icon.to_s, <<~HTML.chomp
-      <div class=\"fatcow-icon fatcow-icon--user\">
-      <img src=\"/images/fatcow/FatCow_Icons32x32/user.png\"><img class=\"fatcow-icon__bullet\" src=\"/images/fatcow/FatCow_Icons32x32/bullet_wrench.png\">
-      </div>
-    HTML
+    assert_matches_snapshot @view.fci(user.form_icon).to_s
   end
 
   test 'custom show icon status get priority over defaults' do
     user = users(:name_icon).dup
-    assert_equal user.show_icon.to_s, <<~HTML.chomp
-      <div class=\"fatcow-icon fatcow-icon--user\">
-      <img src=\"/images/fatcow/FatCow_Icons32x32/user.png\"><img class=\"fatcow-icon__bullet\" src=\"/images/fatcow/FatCow_Icons32x32/bullet_star.png\">
-      </div>
-    HTML
+    assert_matches_snapshot @view.fci(user.show_icon).to_s
   end
 
   test 'custom form icon status get priority over defaults' do
     user = users(:special_icon).dup
-    assert_equal user.form_icon.to_s, <<~HTML.chomp
-      <div class=\"fatcow-icon fatcow-icon--user\">
-      <img src=\"/images/fatcow/FatCow_Icons32x32/user.png\"><img class=\"fatcow-icon__bullet\" src=\"/images/fatcow/FatCow_Icons32x32/bullet_wrench.png\">
-      </div>
-    HTML
+    assert_matches_snapshot @view.fci(user.form_icon).to_s
   end
 
   test 'show icon can support small sizes' do
     user = users(:name_icon)
-    assert_equal user.show_icon(size: :small).to_s, <<~HTML.chomp
-      <div class=\"fatcow-icon fatcow-icon--small fatcow-icon--user\">
-      <img src=\"/images/fatcow/FatCow_Icons16x16/user.png\"><img class=\"fatcow-icon__bullet\" src=\"/images/fatcow/FatCow_Icons16x16/bullet_star.png\">
-      </div>
-    HTML
+    assert_matches_snapshot @view.fci(user.show_icon(size: :small)).to_s
   end
 
   test 'can support oddball statuses that need css hacks' do
     user = users(:name_icon)
-    assert_equal user.custom_icon(:warning).to_s, <<~HTML.chomp
-      <div class=\"fatcow-icon fatcow-icon--user\">
-      <img src=\"/images/fatcow/FatCow_Icons32x32/user.png\"><img class=\"fatcow-icon__bullet fatcow-icon__bullet--hack\" src=\"/images/fatcow/FatCow_Icons16x16/warning.png\">
-      </div>
-    HTML
+    assert_matches_snapshot @view.fci(user.custom_icon(:warning)).to_s
   end
 
   test 'can support small sizes with oddball statuses that need css hacks' do
     user = users(:name_icon)
-    assert_equal user.custom_icon(:warning, size: :small).to_s, <<~HTML.chomp
-      <div class=\"fatcow-icon fatcow-icon--small fatcow-icon--user\">
-      <img src=\"/images/fatcow/FatCow_Icons16x16/user.png\"><img class=\"fatcow-icon__bullet fatcow-icon__bullet--hack\" src=\"/images/fatcow/FatCow_Icons16x16/warning.png\">
-      </div>
-    HTML
+    assert_matches_snapshot @view.fci(user.custom_icon(:warning, size: :small)).to_s
   end
 end
